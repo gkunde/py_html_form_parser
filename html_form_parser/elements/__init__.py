118dd3b48c7e13cf81967d65bacf4ab2b4cdada4
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2021-01-30 15:07:36 -0600

    make is_selected a property
    change is_selected to a property to ensure it contains a boolean value.

--- html_form_parser/elements/__init__.py
+++ html_form_parser/elements/__init__.py
@@ -2,9 +2,16 @@
 
 class FormElement:
 
-    def __init__(self, name: str = None, value: str = None, binary_path: str = None, is_selected: str = None, primary_type: str = None, secondary_type: str = None):
-        """
-        """
+    def __init__(
+            self,
+            name: str = None,
+            value: str = None,
+            binary_path: str = None,
+            is_selected: bool = None,
+            primary_type: str = None,
+            secondary_type: str = None):
+
+        self._is_selected = None
 
         self.name = name
         self.value = value
@@ -12,3 +19,11 @@ class FormElement:
         self.binary_path = binary_path
         self.primary_type = primary_type
         self.secondary_type = secondary_type
+
+    @property
+    def is_selected(self):
+        return self._is_selected
+
+    @is_selected.setter
+    def is_selected(self, value):
+        self._is_selected = bool(value)
